6f8060c10bb51fd7aeab72759f06b441e793ecb6
Bogdan <bogdanstanila2190@gmail.com>
2020-01-28 11:58:46 +0100

    Cambios ignore

---
+++ EjercicioJSON2.js
@@ -0,0 +1,15 @@
+
+var coche1 = {"color":"rojo","arranca":true,"vel.max":200,"interior":["cuero","negro","iluminado","confortable"],
+"motor":{"capacidad":2000,"cambio":"automatico"}};
+
+var coche2 = {"color":"negro","arranca":true,"vel.max":180,"interior":["tapiceria","marron","iluminado","confortable"],
+"motor":{"capacidad":2000,"cambio":"automatico"}};
+
+var coche3 = {"color":"verde","arranca":false,"vel.max":250,"interior":["tapiceria","negro","oscuro","incommodo"],
+"motor":{"capacidad":2000,"cambio":"automatico"}};
+
+
+var caravana = [coche1,coche2,coche3];
+
+document.write(coche1.interior);
+document.write(caravana);
